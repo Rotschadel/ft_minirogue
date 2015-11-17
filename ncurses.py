1e823a036d58838672dff585ff5a61427c1b5f90
Michael YADABA <fyadaba@e1r2p2.42.fr>
2015-11-17 14:10:59 +0100

    Rooms tests.

--- ncurses.py
+++ ncurses.py
@@ -3,42 +3,48 @@ import time
 
 
 class Room(object):
-    def __init__(self, h, w, x, y):
+    def __init__(self, w, h, y, x, path, is_room=True):
         self.height = h
         self.width = w
         self.pos_x = x
         self.pos_y = y
+        self.path = path
+        self.win = curses.newwin(h, w, x, y)
+        self.is_room = is_room
 
-class GUI(object):
+def displayRoom(room):
+    room.win.clear()
+    if room.is_room:
+        room.win.box()
+    else:
+        for p in room.path:
+            stdscr.addstr(p[0], p[1], "p")
+    room.win.refresh()
 
-    def __init__(self):
-        stdscr = curses.initscr()
-        curses.curs_set(False)
-
-    def __del__(self):
-        curses.endwin()
-
-    def display():
-        stdscr.clear()
-        stdscr.addstr(10, 10, "test")
-        time.sleep(2)
-        stdscr.refresh()
-
-#g = GUI()
 stdscr = curses.initscr()
 curses.curs_set(False)
-stdscr.addstr(10, 10, "test")
 stdscr.keypad(1)
+
+rooms = []
+cors = []
+
+rooms.append(Room(5, 7, 1, 1, []))
+rooms.append(Room(8, 8, 10, 10, []))
+"""
+path = [
+    (20, 20),
+    (20, 21),
+    (20, 22),
+    (21, 22),
+    (22, 22),
+]
+rooms.append(Room(5, 5, 15, 15, path, False))
+"""
+
 while True:
     c = stdscr.getch()
-    if c == 27:
+    if c == 27 or c == ord('q'):
         break
-    stdscr.clear()
-    if c == 32:
-        stdscr.addstr(10, 10, "space")
-    elif c < 256:
-        stdscr.addstr(10, 10, "test " + chr(c))
-    else:
-        stdscr.addstr(10, 10, str(c))
-    stdscr.refresh()
+    for r in rooms:
+        displayRoom(r)
 curses.endwin()
